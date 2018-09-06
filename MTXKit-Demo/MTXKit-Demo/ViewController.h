32f629c2e7038542f6a906e9a3e61c5bd7e524fb
MichaelLedger <MichaelLedger@163.com>
2018-09-06 10:39:22 +0800

    Init Xcode Project Demo in Objective-C.

---
+++ MTXKit-Demo/MTXKit-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTXKit-Demo
+//
+//  Created by MountainX on 2018/9/6.
+//  Copyright © 2018年 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
