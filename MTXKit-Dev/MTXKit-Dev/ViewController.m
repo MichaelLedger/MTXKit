4fbeeeab818adae30b29bf0f8b8f17a8c90c73bf
MichaelLedger <MichaelLedger@163.com>
2018-09-17 13:54:50 +0800

    Add Developer Project in Objective-C.

---
+++ MTXKit-Dev/MTXKit-Dev/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  MTXKit-Dev
+//
+//  Created by MountainX on 2018/9/17.
+//  Copyright © 2018年 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "NSObject+MTX.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MTXKit-Dev/MTXKit-DevTests/MTXKit_DevTests.m
@@ -0,0 +1,39 @@
+//
+//  MTXKit_DevTests.m
+//  MTXKit-DevTests
+//
+//  Created by MountainX on 2018/9/17.
+//  Copyright © 2018年 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MTXKit_DevTests : XCTestCase
+
+@end
+
+@implementation MTXKit_DevTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
